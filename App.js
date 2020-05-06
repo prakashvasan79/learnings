430cf1a4162838aab92c4e10ea54838f4ccc5911
Srinivasan, Prakash <prakashvasan@yahoo.com>
2020-05-06 11:33:47 +0530

    testing push once again

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import { Header, Card, Button } from "react-native-elements";
 // Custom components
 import TextItemComp from "./comps/TextItemComp";
 import TextInputComp from "./comps/TextInputComp";
 
 export default function App() {
-  const [ txtList, setTxtList ] = useState([]);
-  const [ launchMode, setLaunchMode ] = useState(false);
+  const [txtList, setTxtList] = useState([]);
+  const [launchMode, setLaunchMode] = useState(false);
 
   const addHandler = (txt) => {
-    if(txt.trim() != "")
-    {
-      setTxtList(curList => [...curList, { id:Math.random().toString(), val:txt }] );
+    if (txt.trim() != "") {
+      setTxtList((curList) => [
+        ...curList,
+        { id: Math.random().toString(), val: txt },
+      ]);
       setLaunchMode(false);
       console.log("Added item : " + txt);
-    }
-    else
-      console.log("Text field is empty !!");
+    } else console.log("Text field is empty !!");
   };
   const deleteHandler = (txtID) => {
-      setTxtList(curList => { return curList.filter( (item) => item.id !== txtID ); });
-      console.log("Removed item : " + txtID); 
+    setTxtList((curList) => {
+      return curList.filter((item) => item.id !== txtID);
+    });
+    console.log("Removed item : " + txtID);
   };
   const cancelHandler = () => {
     setLaunchMode(false);
@@ -28,11 +37,39 @@ export default function App() {
   };
 
   return (
-    <View style={ {padding:30} }>
-      <Button title="ADD CONTENT" onPress={ () => setLaunchMode(true) }/>
-      <TextInputComp launch={launchMode} onAddTxtItem={addHandler} onCancelTxtItem={cancelHandler}/>
-      <FlatList data={txtList} keyExtractor={ (item, index) => item.id }
-        renderItem={ itemData => <TextItemComp id={itemData.item.id} label={itemData.item.val} deleteTxtItem={deleteHandler}/> }/>
+    <View>
+      <Header
+        leftComponent={{ icon: "menu", color: "#fff" }}
+        centerComponent={{ text: "MY BUCKET LIST", style: { color: "#fff" } }}
+      />
+      <Card>
+        <Image
+          style={{ width: "100%", height: 200 }}
+          source={require("./images/bucketlist.jpg")}
+          PlaceholderContent={<ActivityIndicator />}
+        />
+        <Button
+          title="ADD ITEMS TO LIST"
+          onPress={() => setLaunchMode(true)}
+          icon={{ name: "code", color: "white" }}
+        />
+        <TextInputComp
+          launch={launchMode}
+          onAddTxtItem={addHandler}
+          onCancelTxtItem={cancelHandler}
+        />
+        <FlatList
+          data={txtList}
+          keyExtractor={(item, index) => item.id}
+          renderItem={(itemData) => (
+            <TextItemComp
+              id={itemData.item.id}
+              label={itemData.item.val}
+              deleteTxtItem={deleteHandler}
+            />
+          )}
+        />
+      </Card>
     </View>
   );
 }
@@ -41,6 +78,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 30, flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center'
+    padding: 30,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
